6125f00903e281e8349e072d19d8e9cb18a4ad41
Lapis <lapis0875@gmail.com>
2020-11-10 17:48:08 +0900

    feat: Working on Serial commnuication on ArduPilot

--- Assets/Scripts/PilotBehavior.cs
+++ Assets/Scripts/PilotBehavior.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO.Ports;
 using UnityEngine;
@@ -6,16 +7,39 @@ using UnityEngine;
 public class PilotBehavior : MonoBehaviour
 {
     public string portName = "COM7";
-    
-    // Start is called before the first frame update
-    void Start()
+
+    public int baudRate = 9600;
+
+    private SerialPort port;
+
+    // Awake method is called when each objects are load when Scene has been loaded.
+    private void Awake()
     {
         // Detect available ports
         foreach (string portName in SerialPort.GetPortNames())
         {
             Debug.Log(portName);
         }
-        SerialPort port = new SerialPort(portName);
+        SerialPort port = new SerialPort(portName, baudRate);
+        port.DataReceived += new SerialDataReceivedEventHandler(serialport_datareceived);
+        port.Open();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log($"Starting object with port (name={port.PortName},baudrate={port.BaudRate.ToString()})");
+    }
+
+    private void serialport_datareceived(object sender, SerialDataReceivedEventArgs e)
+    {
+        string serialRead = port.ReadLine();
+        Debug.Log($"Read line from serial : {serialRead}");
+        string[] parsed = serialRead.Split('|');
+        foreach (string data in parsed)
+        {
+            Debug.Log($"Parsed serial value : {data}");
+        }
     }
 
     // Update is called once per frame
