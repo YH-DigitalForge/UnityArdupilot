9d2003b8d32a5487903dfc7d16a8defea14ac04c
Lapis <lapis0875@gmail.com>
2020-11-10 17:17:42 +0900

    feat Project Upload

---
+++ Assets/Scripts/PilotBehavior.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO.Ports;
+using UnityEngine;
+
+public class PilotBehavior : MonoBehaviour
+{
+    public string portName = "COM7";
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Detect available ports
+        foreach (string portName in SerialPort.GetPortNames())
+        {
+            Debug.Log(portName);
+        }
+        SerialPort port = new SerialPort(portName);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
